3e573f4f2687f2335879bcd7980bcd648d514c8f
aroille <>
2018-07-22 22:54:45 +1000

    Fix compile issue with VisualStudio2017 in non-debug mode

--- src/ink/math/transform.cpp
+++ src/ink/math/transform.cpp
@@ -17,7 +17,7 @@ namespace ink
     memcpy(m, mat, 16 * sizeof(float));
   }
 
-  inline Matrix4x4::Matrix4x4(float t00, float t01, float t02, float t03,
+  Matrix4x4::Matrix4x4(float t00, float t01, float t02, float t03,
     float t10, float t11, float t12, float t13,
     float t20, float t21, float t22, float t23,
     float t30, float t31, float t32, float t33)
